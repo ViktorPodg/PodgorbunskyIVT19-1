c8f6a7873f5c9f55a4c048b6385e12e2f181c576
ViktorPodg <64357499+ViktorPodg@users.noreply.github.com>
2020-04-26 20:14:08 +0900

    Add files via upload

---
+++ Задание 1 (23.04.2020)/z466.pas
@@ -0,0 +1,9 @@
+﻿procedure Zamen(var s:string);
+var n,i,k:byte;
+begin
+  for i:=k to n do
+    if s[i]='0' then s[i]:='1'
+    else if s[i]='1' then s[i]:='0';
+    writeln('s=',s);
+end.
+
---
+++ Задание 1 (23.04.2020)/z469.pas
@@ -0,0 +1,29 @@
+﻿procedure Stroka(var s:string;d:byte);
+var k,v:byte;
+    i:integer;
+begin
+k:=d-length(s);{сколько добавить}
+v:=0;{вставки пробелов}
+repeat{идем по строке по кругу, добавляю по одному пробелу}
+ for i:=length(s) downto 1 do{с конца строки}
+ if (s[i]=' ')and(s[i-1]<>' ') then{если пробел и перед ним не пробел}
+  begin
+   insert(' ',s,i);{вставляем пробел}
+   v:=v+1;{считаем}
+   if v=k then break;{если все вставили, выходим из цикла}
+  end;
+until v=k;{конец просмотров строки}
+writeln(s);
+end;
+ 
+var s:string;
+    d:byte;
+begin
+writeln('Введите текст:');
+readln(s);
+repeat
+write('Новая длина строки от ',length(s)+1,' до 255  d=');
+readln(d);
+until d in [length(s)+1..255];
+Stroka(s,d);
+end.
